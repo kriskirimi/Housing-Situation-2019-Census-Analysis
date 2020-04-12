714235b5260382e6c5c9d38f962f90cee52ad0fa
kriskirimi <60326060+kriskirimi@users.noreply.github.com>
2020-04-12 13:12:30 +0300

    update

--- Main Dwelling Unit By Mode of Acquisition.py
+++ Main Dwelling Unit By Mode of Acquisition.py
@@ -38,8 +38,8 @@ fig, ax = plt.subplots(figsize=(6.3,6.3))
 #plt.style.use('seaborn')
 textprops={'color:#CFBDA3'}, 
 ax.pie(df1.iloc[:1,1:3], colors=['#838996', '#b2d8d8'], labels=df1.iloc[:1,1:3].columns, autopct='%1.f%%', 
-       radius=1.2, startangle=25, textprops=dict(color = '#000000', fontsize=14, fontname='MingLiU_HKSCS'))
-ax.set_title('Kenya\'s Rural and Urban Homeownership Rates, Census 2019', fontsize=14, fontname='MingLiU_HKSCS')
+       radius=1.2, startangle=25, textprops=dict(color = '#000000', fontsize=14))
+ax.set_title('Kenya\'s Rural and Urban Homeownership Rates, Census 2019', fontsize=14)
 plt.tight_layout()
 
 plt.show()
@@ -64,17 +64,17 @@ barwidth=0.25
 rects1= ax.bar(x, df2['Purchased Number'], width=barwidth)
 rects2=ax.bar(x + barwidth, df2['Mode of Acquisition ConstructedNumber'], width=barwidth)
 rects3=ax.bar(x + barwidth*2, df2['InheritedNumber'], width=barwidth)
-ax.set_xticklabels(df2['County/ Sub-County'].unique(), fontsize=14, fontname='MingLiU_HKSCS')
+ax.set_xticklabels(df2['County/ Sub-County'].unique(), fontsize=14)
 ax.set_xticks(x + barwidth)
-ax.set_xlabel('County', fontsize=14, fontname='MingLiU_HKSCS')
-ax.set_ylabel('% of County Population', fontsize=14, fontname='MingLiU_HKSCS')
-ax.set_title('Counties (hosting major cities) Main Mode of Dwelling Aquisition, Census 2019', fontsize=14, fontname='MingLiU_HKSCS')
+ax.set_xlabel('County', fontsize=14)
+ax.set_ylabel('% of County Population', fontsize=14)
+ax.set_title('Counties (hosting major cities) Main Mode of Dwelling Aquisition, Census 2019', fontsize=14)
 ax.legend(handles=[rects1,rects2, rects3 ], title='Mode of Acquisition', labels=['Purchased','Constructed', 
           'Inherited'], title_fontsize=12, fontsize=12)
 def autolabel(rects):
     for rect in rects:
         yval = rect.get_height()
-        ax.text(rect.get_x(), yval + 1.8, yval, fontsize=12, fontname='MingLiU_HKSCS')
+        ax.text(rect.get_x(), yval + 1.8, yval, fontsize=12)
         
 ax.annotate('Source:KNBS, Census 2019\nCode:https://github.com/kriskirimi/', (0,0), (00,-45), fontsize=10, 
              xycoords='axes fraction', textcoords='offset points')
